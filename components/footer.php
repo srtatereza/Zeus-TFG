2238130da5b32b4c017c3c82263c1bec66355fd2
Vianca Tereza Franco <terezafranco94@gmail.com>
2024-05-27 21:35:57 +0200

    footer

--- components/footer.php
+++ components/footer.php
@@ -2,23 +2,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 footer-col">
-                <h2>Dirección</h2>
+                <h2>Dirección:</h2>
                 <p>
-                    CABA - Argentina <br />
-                    carrasco 400
+                    Espana / Madrid <br />
+                    Calle Principal, Ciudad, País
                 </p>
+                <h2>Telefono:</h2>
+                <p>
+                +54 1234-5678 </p>
             </div>
             <div class="col-md-4 footer-col">
                 <h2>Mis redes</h2>
                 <ul class="list-inline">
                     <li>
-                        <a target="_blank" href="#" class="btn btn-outline-light btn-social"><i class="fab fa-facebook-f"></i></a>
+                        <a target="_blank" href="https://www.facebook.com/" class="btn btn-outline-light btn-social"><i class="fab fa-facebook-f"></i></a>
                     </li>
                     <li>
-                        <a target="_blank" href="#" class="btn btn-outline-light btn-social"><i class="fab fa-twitter"></i></a>
+                        <a target="_blank" href="https://x.com/" class="btn btn-outline-light btn-social"><i class="fab fa-twitter"></i></a>
                     </li>
                     <li>
-                        <a target="_blank" href="#" class="btn btn-outline-light btn-social"><i class="fab fa-instagram"></i></a>
+                        <a target="_blank" href="https://www.instagram.com/" class="btn btn-outline-light btn-social"><i class="fab fa-instagram"></i></a>
                     </li>
                 </ul>
             </div>
@@ -28,7 +31,7 @@
                     <li><a href="#">POLÍTICA SOBRE COOKIES</a></li>
                     <li><a href="#">DECLARACIÓN DE PRIVACIDAD</a></li>
                     <li><a href="#">¿CÓMO PUEDO HACER UN CAMBIO O DEVOLUCIÓN?</a></li>
-                    <li><a href="#">CONÓCENOS</a></li>
+                    <li><a href="/contacto.php">CONÓCENOS</a></li>
                 </ul>
             </div>
         </div>
